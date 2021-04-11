329c1114ec1851e30dea1874f3f082bbcfc2c91b
tkrikoriam <tony.krikorian@outlook.com>
2021-04-11 16:50:54 -0400

    Add yorker page scss

--- pages/newyorker/index.jsx
+++ pages/newyorker/index.jsx
@@ -1,13 +1,14 @@
 import Head from 'next/head'
+import styles from '.'
 import LogoBar from '../../components/logobar/LogoBar'
-import SubscriberBar from '../../components/subscribebar/Index'
+
+
 const Index = () => {
     return (<div className="main-container">
         <Head>
             <title>The New Yorker</title>
             <link rel="shortcut icon" href="yorker_favicon.ico" type="image/x-icon"/>
         </Head>
-        {/* <SubscriberBar newsletter={'#'} login={'#'} search={'#'} subscribe={'#'}/> */}
         <LogoBar/>
     </div>  );
 }
