6d606950416700d59a2446f1cc2950341a43196d
tkrikoriam <tony.krikorian@outlook.com>
2021-04-11 11:22:45 -0400

    Add yorker favicon

---
+++ pages/newyorker/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const Index = () => {
+    return (<div className="main-container">
+        <Head>
+            <title>The New Yorker</title>
+            <link rel="shortcut icon" href="yorker_favicon.ico" type="image/x-icon"/>
+        </Head>
+        <h1>The New Yorker</h1>
+    </div>  );
+}
+ 
+export default Index;
