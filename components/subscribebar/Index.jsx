7617829a04d6d725149f029aa0407f5d5e24f3be
tkrikoriam <tony.krikorian@outlook.com>
2021-04-11 12:10:14 -0400

    Subscribe component

--- components/subscribebar/Index.jsx
+++ components/subscribebar/Index.jsx
@@ -3,8 +3,10 @@ import styles from './SubscribeBar.module.scss'
 
 const SubscribeBar = () => {
     return ( <div className={styles.subscribeBarMainContainer}>
-        <Link href="#"><a>Newsletter</a></Link>
-        <Link href="#"><a>Sign In</a></Link>
+        <Link href="#" ><a className={styles.subscribelink}>Newsletter</a></Link>
+        <Link href="#" ><a className={styles.subscribelink}>Sign In</a></Link>
+        <Link href="#"><a className={styles.subscribebtn}>Subscribe</a></Link>
+        <Link href="#"><span className={styles.searchicon}><img src="./search_black_24dp.svg" alt=""/></span></Link>
     </div> );
 }
  
